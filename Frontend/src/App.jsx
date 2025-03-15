d2f737dfcec99ed3ade9173ba0729695395ac4bf
Parnab Bagchi <pb7439578071@gmail.com>
2025-03-15 18:20:28 +0530

    profile routing testing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -49,6 +49,15 @@ function App() {
                             </>
                         }
                     />
+                    <Route
+                        path="/profile"
+                        element={
+                            <>
+                                <Navbar />
+                                <Footer />
+                            </>
+                        }
+                    />
                     <Route path="/*" element={<Error />} />
                 </Routes>
             </BrowserRouter>
